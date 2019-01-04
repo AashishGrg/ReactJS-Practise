4cd8fbc0e26187df78709d7fa7de5f794b7c5b66
aashish <aashishgurung030@gmail.com>
2019-01-04 16:07:32 +0545

    first API call in react

--- uberdashboard/src/components/Driver.jsx
+++ uberdashboard/src/components/Driver.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
+import axios from "axios";
+const API = "http://192.168.11.98:8000/api/dashboard/driver/list/";
+const DEFAULT_QUERY = "redux";
 
 class Driver extends Component {
+  state = {
+    drivers: []
+  };
+  componentDidMount() {
+    axios
+      .get(`http://192.168.11.98:8000/api/dashboard/driver/list/`)
+      .then(res => {
+        const drivers = res.data;
+        this.setState({ drivers });
+        console.log(drivers);
+      });
+  }
   render() {
     return (
       <div>
-        <h2>Driver</h2>
+        <h2>
+          Hello
+          {/* <ul>
+            {this.state.drivers.map(driver => (
+              <li>{driver.first_name}</li>
+            ))}
+          </ul> */}
+        </h2>
       </div>
     );
   }
